0e6f7b764f11266240f2659a4172cddc7344701a
exectails <exec@dragonbyte.de>
2024-11-26 12:39:11 +0100

    Standardized node list

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 from .nodes.dynamic_prompt import ETDynamicPrompt
 
-et_nodes = [
-    ("Dynamic Prompt", ETDynamicPrompt),
-]
+NODE_CLASS_MAPPINGS = {
+    "ETDynamicPrompt": ETDynamicPrompt,
+}
 
-NODE_CLASS_MAPPINGS = {cls.__name__: cls for display_name, cls in et_nodes}
-NODE_DISPLAY_NAME_MAPPINGS = {cls.__name__: display_name for display_name, cls in et_nodes}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ETDynamicPrompt": "Dynamic Prompt",
+}
 
-WEB_DIRECTORY = "./js"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
